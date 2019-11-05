1474d202bb7d9b9a1136cc96393fb1499a1afeed
Ian <barrowian1991@gmail.com>
2019-11-05 16:27:33 +0000

    Completed movement, interaction and gauge prototype
    Refined player movement prototype, interaction with distance grabbing prototype, and gauge UI prototype

---
+++ ProjectHephaestus/Assets/Scripts/ChangeMaterialColour.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeMaterialColour : MonoBehaviour
+{ 
+    [SerializeField] Color colour;
+
+    void Start()
+    {
+        GetComponent<Renderer>().material.color = colour;
+    }
+}
---
+++ ProjectHephaestus/Assets/Scripts/GaugeManager.cs
@@ -0,0 +1,30 @@
+﻿using OculusSampleFramework;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GaugeManager : MonoBehaviour
+{
+    [SerializeField] private GameObject _anvilTimerGauge;
+
+    private void Start()
+    {
+        _anvilTimerGauge.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<DistanceGrabbable>() == true)
+        {
+            _anvilTimerGauge.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.GetComponent<DistanceGrabbable>() == true)
+        {
+            _anvilTimerGauge.SetActive(false);
+        }
+    }
+}
