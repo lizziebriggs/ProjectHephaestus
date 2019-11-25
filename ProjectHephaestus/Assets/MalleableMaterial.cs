3f75671ff131b81b71c9734a90a164cd88887b25
Lizzie <lizzie.briggs10@gmail.com>
2019-11-25 15:24:31 +0000

    Hammering mechanic
    Hammering mechanic working

---
+++ ProjectHephaestus/Assets/MalleableMaterial.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MalleableMaterial : MonoBehaviour
+{
+    [SerializeField] int hitCount;
+    [SerializeField] GameObject hammered;
+
+    //public bool isMalleable;
+
+    private int hitCounter;
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+
+        if (collision.gameObject.GetComponent<HammerController>())
+        {
+            Debug.Log(hitCounter);
+            hitCounter++;
+
+            if (hitCounter == hitCount)
+            {
+                GameObject hammeredObject = Instantiate(hammered);
+                hammeredObject.transform.position = transform.position;
+
+                Destroy(gameObject);
+            }
+        }
+    }
+}
