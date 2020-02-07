29ce2b9af4136d05b68b49b62ee158d74f14432e
Lizzie <lizzie.briggs10@gmail.com>
2020-02-07 18:42:30 +0000

    Completed hub with level transitioning

---
+++ ProjectHephaestus/Assets/Scripts/LevelItem.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelItem : MonoBehaviour
+{
+    [SerializeField] private int _level;
+    public int Level => _level;
+}
---
+++ ProjectHephaestus/Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    private enum LevelState { Idle, LevelLoaded }
+    [SerializeField] private Transform _objectPoint;
+    private LevelItem _currentLevel;
+    private LevelState _currentState = LevelState.Idle;
+    private int _speed = 50;
+
+    private void Update()
+    {
+        switch (_currentState)
+        {
+            default:
+                break;
+            case LevelState.Idle:
+                break;
+            case LevelState.LevelLoaded:
+                LevelLoadedState();
+                break;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        var level = other.GetComponent<LevelItem>();
+        if (level)
+        {
+            if (!_currentLevel)
+            {
+                _currentLevel = level;
+                _currentState = LevelState.LevelLoaded;
+                SceneManager.LoadScene(level.Level, LoadSceneMode.Additive);
+            }
+
+            else if (_currentLevel != level)
+            {
+                SceneManager.UnloadSceneAsync(_currentLevel.Level);
+                SceneManager.LoadScene(level.Level, LoadSceneMode.Additive);
+                _currentLevel = level;
+                _currentState = LevelState.LevelLoaded;
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        var level = other.GetComponent<LevelItem>();
+        if (level)
+        {
+
+
+            if (_currentLevel)
+            {
+                SceneManager.UnloadSceneAsync(_currentLevel.Level);
+                _currentState = LevelState.Idle;
+                _currentLevel = null;
+            }
+        }
+    }
+
+    private void LevelLoadedState()
+    {
+        if (_currentLevel) // if anvil object isn't null
+        {
+            var levelRigidbody = _currentLevel.GetComponent<Rigidbody>(); // get the rigidbody
+            if (!levelRigidbody.isKinematic) // when the object is not being held
+            {
+                _currentLevel.gameObject.transform.up = transform.up; // set up rotation
+                _currentLevel.gameObject.transform.position = transform.position;// hold position on the anvil
+
+            }
+            _currentLevel.gameObject.transform.Rotate(0, 0, _speed * Time.deltaTime);
+        }
+    }
+}
